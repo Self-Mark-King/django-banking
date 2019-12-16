d185eefa88715619176d60f20273fe67ffc80cba
Ceiphid <Aqkeem@gmail.com>
2019-12-16 15:10:09 -0800

    fix cors headers

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 from django.urls import path, include                 # add this
 from rest_framework import routers                    # add this
-from todo import views                            # add this
+from todo import views                               # add this
         
 router = routers.DefaultRouter()                      # add this
 router.register(r'todos', views.TodoView, 'todo')     # add this
